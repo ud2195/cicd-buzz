1f7c7aa91ebd42224e07c091dab44e4208acfe57
ESPM1854 <udaykhanna2195@gmail.com>
2020-09-20 02:31:37 +0530

    initial commit

---
+++ buzz/generator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 01:46:24 2020
+
+@author: espm1854
+"""
+from __future__ import print_function
+import random
+
+buzz = ('continuous testing', 'continuous integration',
+    'continuous deployment', 'continuous improvement', 'devops')
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+    'seriously')
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
+def sample(l, n = 1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+        sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
+if __name__ == "__main__":
+    print(generate_buzz())
